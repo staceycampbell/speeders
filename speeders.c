c349174a1279a06355f2dcffa95248242e8088cc
Stacey Campbell <staceycampbell@yahoo.com>
2022-06-12 13:49:52 -0700

    Set up Mastodon bot at botsin.space using Terence Eden's handy instructions at https://shkspr.mobi/blog/2018/08/easy-guide-to-building-mastodon-bots/

--- speeders.c
+++ speeders.c
@@ -6,6 +6,8 @@
 #include <time.h>
 #include <math.h>
 #include <getopt.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 // Upper left and lower right coordinates of area where speeders
 // will be reported
@@ -22,10 +24,12 @@
 #define FAA_SPEED_LIMIT 250 // FAA speed limit in kt
 #define FAA_SPEED_ALTITUDE 10000 // ...at or below this MSL altitude in ft
 
-#define NAUGHTY_SPEED (FAA_SPEED_LIMIT + 25) // too fast!
-#define NAUGHTY_ALTITUDE (FAA_SPEED_ALTITUDE - 1000) // too low!
+#define NAUGHTY_SPEED (FAA_SPEED_LIMIT + 25) // too fast at or below NAUGHTY_ALTITUDE!
+#define NAUGHTY_ALTITUDE (FAA_SPEED_ALTITUDE - 1000)
 
 #define PLANE_COUNT 1024 // never more than about 70 planes visible from the Valley
+
+static const char BotToken[] = "token.secret";
 
 typedef struct fastest_t {
 	double naughty;
@@ -102,6 +106,9 @@ RecordBadPlane(plane_t *plane)
 
 	if (plane->altitude <= 0)
 		return; // yikes
+
+	if (plane->speed >= 600)
+		return; // bad squitter
 
 	// zone of interest is the rectangle defined by the xx_LAT,xx_LON defines plus within Y miles of ZeroLat,ZeroLon
 	lat_radians = DegreesToRadians(plane->latitude);
@@ -280,8 +287,14 @@ ProcessPlane(char **pp, plane_t planes[PLANE_COUNT], uint32_t message_id, uint32
 }
 
 static void
-ReportBadPlane(plane_t *plane)
+ReportBadPlane(plane_t *plane, int enable_bot)
 {
+	FILE *fp;
+	int system_status;
+	char filename[256];
+	char command[1024];
+	static int fn_inc = 0;
+	
 	printf("%X %s %d %d %4.1f %8.4f %8.4f (nv %4.1f) %s",
 	       plane->icao,
 	       plane->callsign,
@@ -292,11 +305,37 @@ ReportBadPlane(plane_t *plane)
 	       plane->fastest.longitude,
 	       plane->fastest.naughty,
 	       ctime(&plane->fastest.seen));
+	if (enable_bot)
+	{
+		sprintf(filename, "/tmp/bisbadplane%ld-%d.py", (long int)getpid(), fn_inc);
+		++fn_inc;
+		if ((fp = fopen(filename, "w")) == 0)
+		{
+			perror(__PRETTY_FUNCTION__);
+			exit(1);
+		}
+		fprintf(fp, "from mastodon import Mastodon\n");
+		fprintf(fp, "mastodon = Mastodon(\n    access_token = '%s',\n    api_base_url = 'https://botsin.space/'\n)\n", BotToken);
+		fprintf(fp, "mastodon.status_post(\"BLEEP BLOOP: I just saw an aircraft with callsign %s (ICAO code %X) flying at %d kt at altitude %d feet MSL at "
+			"coordiantes %8.4f,%8.4f.\\nhttps://globe.adsbexchange.com/?icao=%x\")\n",
+			plane->callsign,
+			plane->icao,
+			plane->fastest.speed,
+			plane->fastest.altitude,
+			plane->fastest.latitude,
+			plane->fastest.longitude,
+			plane->icao);
+		fclose(fp);
+		sprintf(command, "/usr/bin/python3 %s", filename);
+		system_status = system(command);
+		if (system_status)
+			fprintf(stderr, "%s: system(%s) returned status %d\n", __PRETTY_FUNCTION__, command, system_status);
+	}
 }
 
 
 static void
-CleanPlanes(plane_t planes[PLANE_COUNT])
+CleanPlanes(plane_t planes[PLANE_COUNT], int enable_bot)
 {
 	int i;
 	time_t now, duration;
@@ -310,7 +349,7 @@ CleanPlanes(plane_t planes[PLANE_COUNT])
 			if (duration > 10)
 			{
 				if (planes[i].speeder)
-					ReportBadPlane(&planes[i]);
+					ReportBadPlane(&planes[i], enable_bot);
 				planes[i].valid = 0;
 			}
 		}
@@ -320,12 +359,44 @@ CleanPlanes(plane_t planes[PLANE_COUNT])
 int
 main(int argc, char *argv[])
 {
-	int i;
+	int i, opt, enable_bot, usage;
 	uint32_t message_id, icao;
 	char buffer[1024];
 	char *p;
 	char *ch;
 	plane_t planes[PLANE_COUNT];
+
+	enable_bot = 0;
+	usage = 0;
+	while ((opt = getopt(argc, argv, "b")) != EOF)
+		switch (opt)
+		{
+		case 'b' :
+			enable_bot = 1;
+			break;
+		default :
+			usage = 1;
+			break;
+		}
+	if (usage)
+	{
+		fprintf(stderr, "usage: %s [-b]\n", argv[0]);
+		fprintf(stderr, "\t-b = enable bot reporting\n\n");
+		fprintf(stderr, "\texample usage: nc localhost 30003 | %s\n", argv[0]);
+		
+		return 1;
+	}
+
+	if (enable_bot)
+	{
+		struct stat statbuf;
+		
+		if (stat(BotToken, &statbuf))
+		{
+			fprintf(stderr, "%s: cannot stat bot token file %s\n", argv[0], BotToken);
+			exit(1);
+		}
+	}
 
 	for (i = 0; i < PLANE_COUNT; ++i)
 		planes[i].valid = 0;
@@ -356,7 +427,7 @@ main(int argc, char *argv[])
 				}
 			}
 		}
-		CleanPlanes(planes);
+		CleanPlanes(planes, enable_bot);
 		DetectBadPlanes(planes);
 	}
 
