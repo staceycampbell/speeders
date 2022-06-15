ef1e58645653e81174833ceb1bbc75af419bc8bd
Stacey Campbell <staceycampbell@yahoo.com>
2022-06-14 17:10:07 -0700

    Convert calibrated airspeed to true airspeed. Guesstimate static air temp.

---
+++ castotas.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+#include "castotas.h"
+
+// Calibrated Air Speed to True Air Speed
+// https://aviation.stackexchange.com/a/64251
+
+int32_t
+CAStoTAS(int32_t cas, int32_t altitude)
+{
+	int32_t tas;
+	double cas_mps;
+	double h;
+	double T;
+	double tas_mps;
+	static const double a0 = 340.3; // m/s is the speed of sound at sea level in the ISA,
+	static const double g = 9.80665; // m/s2 is the standard acceleration due to gravity,
+	static const double L = 0.0065; // K/m is the standard ISA temperature lapse rate,
+	static const double M = 0.0289644; // kg/mol is the molar mass of dry air,
+	static const double R = 8.3144598; // J/(mol⋅K) is the universal gas constant,
+	static const double T0 = 288.15; // K is the static air temperature at sea level in the ISA.
+
+	cas_mps = (double)cas * 0.514444; // calibrate airspeed m/s
+	h = (double)altitude * 0.3048; // altitude in m
+
+	// https://www.grc.nasa.gov/www/k-12/airplane/atmosmet.html
+	T = 15.04 - 0.00649 * h + 273.15; // approx static air temp in K
+
+	double Lh_div_T0 = (L * h) / T0;
+	double neg_gM_div_RL = -((g * M) / (R * L));
+	double cas_squared_div_5a0_squared = pow(cas_mps, 2.0) / (5.0 * pow(a0, 2.0));
+	double sevenRT_div_M = (7.0 * R * T) / M;
+
+	double exp0 = pow(cas_squared_div_5a0_squared + 1, 7.0 / 2.0) - 1;
+	double exp1 = pow(1.0 - Lh_div_T0, neg_gM_div_RL);
+	double exp2 = exp0 * exp1 + 1;
+	double exp3 = pow(exp2, 2.0 / 7.0) - 1;
+	double exp4 = sevenRT_div_M * exp3;
+
+	tas_mps = sqrt(exp4);
+	tas = tas_mps * 1.94384 + 0.5;
+
+	return tas;
+}
---
+++ castotas.h
@@ -0,0 +1,1 @@
+extern int32_t CAStoTAS(int32_t cas, int32_t altitude);
